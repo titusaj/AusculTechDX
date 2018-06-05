9fc93ac07b4827497015e575c6608c26350a83c8
enyu0510 <39782973+enyu0510@users.noreply.github.com>
2018-06-05 10:27:38 -0400

    adding nav color change js

---
+++ HTML4/js/navColorChange.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
+
+});
